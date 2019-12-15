778b64c4f588bf83403d29327f8e33a2403464cf
Paul Brady <pablo.brady@gmail.com>
2019-12-15 14:37:14 -0800

    Display the event title, in _id.vue

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Event #{{ id }}</h1>
+    <h1>{{ event.title }}</h1>
   </div>
 </template>
 
@@ -8,19 +8,27 @@
 export default {
   head() {
     return {
-      title: 'Event #' + this.id,
+      title: this.event.title,
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: 'What you need to know about event #' + this.id
+          content: 'What you need to know about event ' + this.event.title
         }
       ]
     }
   },
-  computed: {
-    id() {
-      return this.$route.params.id
+  async asyncData({ $axios, params, error = null }) {
+    try {
+      const { data } = await $axios.get('http://localhost:3000/events/' + params.id )
+      return {
+        event: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch event #' + params.id + '.  Sorry dude, dudess, or non-binary dude-person.'
+      })
     }
   }
 }
