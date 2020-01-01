def9a3226ef2b35eab98a96033b8324edf7f222d
Paul Brady <pablo.brady@gmail.com>
2019-12-31 16:49:20 -0800

    Static Page Generation

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+import EventService from "./services/EventService"
 
 export default {
   mode: 'universal',
@@ -57,6 +58,15 @@ export default {
     ** You can extend webpack config here
     */
     extend (config, ctx) {
+    }
+  },
+  generate: {
+    routes: () => {
+      return EventService.getEvents().then(response => {
+        return response.data.map(event => {
+          return '/event/' + event.id
+        })
+      })
     }
   }
 }
